dc5f527dc2d4f894d3ff939c6343fad4fcc3cc97
VladGamanin <vladg71@gmail.com>
2020-08-16 20:22:04 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,13 +1,12 @@
+{ config, pkgs}:
+
 {
      imports = [ <nixpkgs/nixos/modules/virtualisation/amazon-image.nix> ];
      ec2.hvm = true;
- }
-
- {pkgs = import <nixpkgs> {};
-     
      environment.systemPackages = with pkgs; [
              htop
              mc
              vim
              ];
-}
+ }
+
