cfebd04a4561bd0f258d764f8ba53bb81f9dfd2c
VladGamanin <vladg71@gmail.com>
2020-08-20 09:51:38 +0300

    Create configuration-nginx.nix

---
+++ configuration-nginx.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, services,...}:
+{
+imports = [ <nixpkgs/nixos/modules/virtualisation/amazon-image.nix> ];
+     ec2.hvm = true;
+     environment.systemPackages = with pkgs; [
+             htop
+             mc
+             vim
+
+             ];
+security.acme.acceptTerms = true;
+security.acme.email = "adminacme@shadowside.tk";
+services.nginx = {
+  enable = true;
+  virtualHosts."shadowside.tk" = {
+    enableACME = true;
+    forceSSL = true;
+    root = "/var/www/shadowside.tk";
+  };
+};
+# Optional: You can configure the email address used with Let's Encrypt.
+# This way you get renewal reminders (automated by NixOS) as well as expiration emails.
+security.acme.certs = {
+  "shadowside.tk".email = "admin@shadowside.tk";
+};
+
+}
